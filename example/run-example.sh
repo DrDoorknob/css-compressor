f6ca9204f1b252e6dac37ed06b0fdbb933fa54dd
DrDoorknob <katananokage@gmail.com>
2015-01-08 10:10:39 -0500

    Removed example from previous project fork, to be re-added later

--- example/run-example.sh
+++ example/run-example.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
-# running of this script will create catalog example/min
-# containing compiled CSS according to example/config.json
-
-java -jar ../build/css-compressor.jar config.json
+# TODO: This example is incomplete, and may / may not be added later.
+# Base sample, where directory structure is:
+# wwwroot
+#   css
+#   static
+#     css
+# java -jar css-relativity.jar -root wwwroot -inpath css -outpath "static/css" -outwrapper "/*
+# * @copyright 2015 Banana Studios. All Rights Reserved.
+# */
+#
+#%output%"
